f3bb8db85f4c1bba02762fa2ecd678d11836febc
Beatriz Carolina Okabe <beatriz.okabe@azi.com.br>
2023-11-20 14:05:13 -0400

    Tutorial do capítulo 3 do Curso para desenvolvimento de jobs com Spring Batch
    - Como capturar variáveis do ambiente de execução por meio de jobParameters em um escopo da step

--- primeiroprojetospringbatch/src/main/java/com/br/udemy/primeiroprojetospringbatch/BatchConfig.java
+++ primeiroprojetospringbatch/src/main/java/com/br/udemy/primeiroprojetospringbatch/BatchConfig.java
@@ -5,11 +5,13 @@ import org.springframework.batch.core.Step;
 import org.springframework.batch.core.StepContribution;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepScope;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -21,8 +23,8 @@ public class BatchConfig {
     @Autowired
     private StepBuilderFactory stepBuilderFactory;
 
-    /** O uso do Incrementer não é recomendado em ambientes de produção, pois ele interrompe e reinicializa um Job a partir do zero.
-    Isso pode ser indesejável em um ambiente de produção, já que o progresso de execução do Job é perdido.**/
+    /** O uso do Incrementer não é recomendado em ambientes de produção, pois interrompe e reinicializa um Job a partir do zero.
+    Isso pode ser indesejável no ambiente de produção, já que o progresso de execução do Job é perdido.**/
 
     @Bean
     public Job imprimeHelloWorldJob() {
@@ -36,14 +38,22 @@ public class BatchConfig {
     private Step imprimeHelloWorldStep() {
         return stepBuilderFactory
                 .get("imprimeHelloWorldStep")
-                .tasklet(new Tasklet() {
-                    @Override
-                    public RepeatStatus execute(StepContribution stepContribution, ChunkContext chunkContext) throws Exception {
-                        System.out.println("Hello World");
-                        return RepeatStatus.FINISHED;
-                    }
-                })
+                .tasklet(helloWorldTasklet(null))
                 .build();
     }
 
+
+    @Bean
+    @StepScope
+    public Tasklet helloWorldTasklet(@Value("#{jobParameters['nome']}") String nome) {
+        return new Tasklet() {
+            @Override
+            public RepeatStatus execute(StepContribution stepContribution, ChunkContext chunkContext) throws Exception {
+                //O nome em jobParameters é o argumento capturado em tempo de execução na aplicação.
+                // Para ter acesso ao jobParameters, o método precisa ser reconhecido como um componente do Spring Batch e precisa ser reconhecido dentro do contexto da aplicação.
+                System.out.printf("Hello %s!%n", nome);
+                return RepeatStatus.FINISHED;
+            }
+        };
+    }
 }
