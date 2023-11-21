f55a7e5d26ae9d0dfc460a8b71af17cb80d71c8d
Beatriz Carolina Okabe <beatriz.okabe@azi.com.br>
2023-11-21 13:43:15 -0400

    Capítulo 5 - ArquivoLarguraFixaJob

--- ArquivoLarguraFixaJob/src/main/java/com/springbatch/arquivolargurafixa/writer/LeituraArquivoLarguraFixaWriterConfig.java
+++ ArquivoLarguraFixaJob/src/main/java/com/springbatch/arquivolargurafixa/writer/LeituraArquivoLarguraFixaWriterConfig.java
@@ -8,8 +8,25 @@ import com.springbatch.arquivolargurafixa.dominio.Cliente;
 
 @Configuration
 public class LeituraArquivoLarguraFixaWriterConfig {
-	@Bean
-	public ItemWriter<Cliente> leituraArquivoLarguraFixaWriter() {
-		return items -> items.forEach(System.out::println);
-	}
+
+    @Bean
+    public ItemWriter<Cliente> leituraArquivoLarguraFixaWriter() {
+        return items -> items.forEach(System.out::println);
+    }
+
+    /**Nesse exemplo está forçando o lançamento de um erro. Quando o cliente com nome "Maria" é detectado, a leitura é interrompida. É possível olhar
+     * o ponteiro que salva onde parou o Step na tabela batch_step_execution_context. Caso a Maria faça parte de um chunk maior do que 2, o chunk inteiro será reinicializado ao
+     * executar novamente a operação **/
+//	@Bean
+//	public ItemWriter<Cliente> leituraArquivoLarguraFixaWriter() {
+//		return items -> items.forEach(System.out::println);
+//		return items -> {
+//			for (Cliente cliente : items) {
+//				if (cliente.getNome().equals("Maria"))
+//					throw new Exception();
+//				else
+//					System.out.println(cliente);
+//			}
+//		};
+//	}
 }
--- init-scripts/CUIDADO_SQL_LIMPAR_SPRING_BATCH.sql
+++ init-scripts/CUIDADO_SQL_LIMPAR_SPRING_BATCH.sql
@@ -1,5 +1,6 @@
 -- DELETE FROM spring_batch.public.batch_job_execution_context WHERE job_execution_id is not null;
 -- DELETE FROM spring_batch.public.batch_step_execution_context WHERE batch_step_execution_context.step_execution_id is not null;
 -- DELETE FROM spring_batch.public.batch_step_execution WHERE batch_step_execution.step_execution_id is not null;
+-- DELETE FROM spring_batch.public.batch_job_execution_params WHERE job_execution_id is not null;
 -- DELETE FROM spring_batch.public.batch_job_execution WHERE batch_job_execution.job_execution_id is not null;
 -- DELETE FROM spring_batch.public.batch_job_instance WHERE job_instance_id is not null;
