f943152aeeb7b50283d668a3c0595f29a5b0ca13
Beatriz Carolina Okabe <beatriz.okabe@azi.com.br>
2023-11-22 09:49:17 -0400

    Seção 7. Escritores - Escrita em arquivo flat formatado

--- writers/ArquivoLarguraFixaJob/src/main/java/com/springbatch/arquivolargurafixa/writer/ArquivoLarguraFixaWriterConfig.java
+++ writers/ArquivoLarguraFixaJob/src/main/java/com/springbatch/arquivolargurafixa/writer/ArquivoLarguraFixaWriterConfig.java
@@ -1,16 +1,34 @@
 package com.springbatch.arquivolargurafixa.writer;
 
-import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.core.configuration.annotation.StepScope;
+import org.springframework.batch.item.file.FlatFileItemWriter;
+import org.springframework.batch.item.file.builder.FlatFileItemWriterBuilder;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import com.springbatch.arquivolargurafixa.dominio.Cliente;
+import org.springframework.core.io.Resource;
+
 
 @Configuration
 public class ArquivoLarguraFixaWriterConfig {
-	// FIXME: Modificar para escrever em arquivo de largura fixa!
-	@Bean
-	public ItemWriter<Cliente> leituraArquivoLarguraFixaWriter() {
-		return items -> items.forEach(System.out::println);
-	}
+    @Bean
+    @StepScope
+    public FlatFileItemWriter<Cliente> escritaArquivoLarguraFixaWriter(
+            @Value("#{jobParameters['arquivoClientesSaida']}") Resource arquivoClientesSaida) {
+		return new FlatFileItemWriterBuilder<Cliente>()
+                .name("escritaArquivoLarguraFixaWriter")
+                .resource(arquivoClientesSaida)
+                .formatted()
+                //se a string não tiver tamanho especificado, ele completa com espaço
+                //por padrão o formato sempre acontece do lado ESQUERDO, para que ele reflita para
+                // o lado DIREITO do char, precisa ser colocado um "-" antes do número
+                .format("%-9s %-9s %-2s %-19s")
+                .names(new String[] {"nome", "sobrenome", "idade", "email"})
+                .build();
+    }
+
+//.columns(new Range[]{new Range(1,10), new Range(11, 20), new Range(21, 23), new Range(24, 43)})
+    //10 - 1 = 9, 20-11 = 9, 23 - 21 = 2, 43-24 = 19
 }
